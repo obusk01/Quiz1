d448fab4394584c1e8a0fc71b1163fea844be9fe
Olivia Busk <123026517+obusk01@users.noreply.github.com>
2023-01-27 11:39:52 -0500

    Add files via upload

---
+++ Quiz1.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#problem 4
+2+(5-3)**5*27/(4*3)
+74.0
+# problem 5
+print("Be not afraid of greatnes", \n  "Some are born great, some achieve greatness, and others have greatness thrust upon them")
+SyntaxError: unexpected character after line continuation character
+print("Be not afraid of greatnes, \n  Some are born great, some achieve greatness, and others have greatness thrust upon them")
+Be not afraid of greatnes, 
+  Some are born great, some achieve greatness, and others have greatness thrust upon them
+print("Be not afraid of greatnes, \nSome are born great, some achieve greatness, and others have greatness thrust upon them")
+Be not afraid of greatnes, 
+Some are born great, some achieve greatness, and others have greatness thrust upon them
